5b81aad71c30620bbf94a791c0912f72ec8daade
Aishwarya <rraishwarya5@gmail.com>
2026-02-06 16:07:32 +0530

    helloo

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 
 const studentRoutes = require('./Router/route');
-app.use('/students', (req,res)=>{
-    res.send("hwlloooo")
-});
+app.use('/students', studentRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port http://localhost:3000');
